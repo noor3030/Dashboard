09f652eaa3a20e7ffe0e2b34feb17b0fd4fd70bf
M Atheer <masreplay@gmail.com>
2022-03-11 15:49:38 +0300

    Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-// import { createVuetify } from "vuetify";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 
-const app = createApp(App);
-app.use(store);
-app.use(router);
+loadFonts();
 
-app.mount("#app");
+createApp(App).use(router).use(store).use(vuetify).mount("#app");
