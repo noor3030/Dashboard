00dfce2eeb8b815356934a3daf77c936fd4d5fe8
M Atheer <masreplay@gmail.com>
2022-03-15 10:47:52 +0300

    Fix prettier

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,35 +1,40 @@
 <template>
   <v-card v-for="user in users.results" :key="user.id">
+    <v-card-media :src="user.image" height="200px"> </v-card-media>
     <v-card-title primary-title>
       <div>
-        <a
-          ><h3 class="headline mb-0">{{ user.name }}</h3></a
-        >
-        <div>description</div>
+        <h3 class="headline mb-0">{{ user.name }}</h3>
+        <div>{{ user.email }}</div>
       </div>
     </v-card-title>
-    <v-avatar size="100PX" color="red">
-      <img :src="user.image" :alt="user.name" />
-    </v-avatar>
+    <v-card-actions>
+      <v-btn flat color="primary">text</v-btn>
+      <v-btn flat color="primary">text</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script lang="ts">
 import { UsersService } from "@/client";
 import { Paging_User_ as Users } from "@/client/models/Paging_User_";
-import { Vue } from "vue-class-component";
 
-export default class UsersController extends Vue {
-  users: Users = { results: [], count: 0 };
+export default {
+  data() {
+    return {
+      users: {} as Users,
+    };
+  },
 
-  getUsers() {
-    UsersService.readUsersV1UsersGet().then((value) => {
-      this.users = value;
-    });
-  }
+  methods: {
+    getUsers() {
+      UsersService.readUsersV1UsersGet().then((value) => {
+        this.users = value;
+      });
+    },
+  },
 
   created() {
     this.getUsers();
-  }
-}
+  },
+};
 </script>
